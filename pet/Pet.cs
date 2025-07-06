b00931d1e78fab4a445024628e03e1c231b3dfb5
Markéta Velková <mark.velkova@seznam.cz>
2025-07-06 15:11:04 +0200

    added pet feeding

--- pet/Pet.cs
+++ pet/Pet.cs
@@ -17,6 +17,12 @@
         // full is 100, cannot be more
         private int MoodMeter { get; set; }
         // full is 100, cannot be more
+        public enum FeedingResult { Fell, Successful, NoFood }
+        private int _foodFellChance = 5;
+        private int _minFoodFed = 1;
+        private int _maxFoodFed = 4;
+        private Random rnd = new Random();
+
 
         //creates new pet
         public Pet() 
@@ -31,5 +37,21 @@
         //creates pet from file
         public Pet(string filename) => throw new NotImplementedException();
 
+        #region feeding
+        public FeedingResult TryFeed()
+        {
+            if (FoodCount == 0)
+                return FeedingResult.NoFood;
+            int fallResult = rnd.Next(100);
+            if (fallResult <= _foodFellChance)
+                return FeedingResult.Fell;
+            return FeedingResult.Successful;
+        } 
+        private void Feed()
+        {
+            int amount = rnd.Next(_minFoodFed,_maxFoodFed);
+            HungerMeter += amount;
+        }
+        #endregion
     }
 }
