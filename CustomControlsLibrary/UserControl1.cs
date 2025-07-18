cfdb84cd753f06124c4372b6e118362dce87771a
Markéta Velková <mark.velkova@seznam.cz>
2025-07-18 21:01:38 +0200

    pridano custom tlacitko doopravdy funkcne

---
+++ CustomControlsLibrary/UserControl1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControlsLibrary
+{
+    /// <summary>
+    /// My own button class used everywhere
+    /// </summary>
+    public class MyButton : Button
+    {
+        public MyButton()
+        {
+            this.Font = new Font("Bahnschrift", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowOnly;
+        }
+    }
+}
---
+++ WinFormsControlLibrary1/MyButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsControlLibrary1
+{
+    public class MyButton : Button
+    {
+        public MyButton()
+        {
+            this.Font = new Font("Bahnschrift", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowOnly;
+        }
+    }
+}
